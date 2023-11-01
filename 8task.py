0f3b91f75e12fdc1657b2377d809490c26a1d671
Helen <gubinaelenaaleksandrovna@gmail.com>
2023-11-01 16:51:37 +0200

    8 завдання

--- 8task.py
+++ 8task.py
@@ -1,9 +1,15 @@
 def num_generator(n):
     for i in range(1, n):
-        yield i
+        prime = True
+        for j in range(1, i):
+            if i % j == 0 and i != j and j != 1:
+                prime = False
+                break
+        if prime:
+            yield i
 
 
-num_gen = num_generator(17)
+num_gen = num_generator(18)
 while True:
     try:
         print(next(num_gen))
